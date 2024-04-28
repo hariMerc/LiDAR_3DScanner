19e9a50a95a2ee9efdbae6086b59fe86cfb8ca53
hariMerc <168324252+hariMerc@users.noreply.github.com>
2024-04-28 12:49:43 +0530

    Update control.ino
    Almost done with it, might need to change the angles with which the pulse width is mapped to, won't know if its correct until we take the readings.

--- ArduinoCode/control.ino
+++ ArduinoCode/control.ino
@@ -2,7 +2,7 @@
 #include<SoftwareSerial.h>
 #include "TFMini.h"
 
-#define LIDAR_MIN_DIST 0
+#define LIDAR_MIN_DIST 20
 #define LIDAR_MAX_DIST 800 // in cm
 #define SAMPLES 5
 
@@ -13,20 +13,90 @@ Servo servoTheta; //Horizontal servo
 SoftwareSerial mySerial(10, 11); //Defines the TFMini TX pin and TFMini RX pin
 TFMini tfmini; //Object for the LiDAR sensor
 
-int dist; //holds the distance(in cm)
+uint16_t dist; //holds the distance(in cm)
 int i;
+int pointArray[3]; //Holds the 0. hAngle 1. vAngle 2. distance
+
+int angle; 
+int verAngle; 
+int verAngleStop;
+int angleFrom; 
+int angleTo; 
 
 
 void setup()
 {
 	Serial.begin(115200);
+	Serial3.begin(115200);
+	mySerial.begin(TFMINI_BAUDRATE);
+	tfmini.begin(&mySerial);
 	servoAlpha.attach(9);
 	servoTheta.attach(10);
+	
+	//Horizontal Servo motion on startup
+	servoTheta.writeMicroseconds(2000);//left
+	delay(950);
+	servoTheta.writeMicroseconds(1000);//right
+	delay(950);
+	servoTheta.writeMicroseconds(1500);//middle
+	delay(950);
+	//Vertical servo motion on startup
+	servoAlpha.writeMicroseconds(1500);
+	delay(950);
+	servoAlpha.writeMicroseconds(1700);
+	delay(950);
+	servoAlpha.writeMicroseconds(1000);
+	delay(950);
+	
+	
+}
+
+void loop()
+{
+	
+	if (verAngle >= verAngleStop) {
+
+		for (angle = angleFrom; angle <= angleTo; angle += 7)
+		{
+		  pointArray[2] = SmoothLidarReading();
+		  mapAngle = angle;
+		  mapAngle = map(mapAngle, 500, 2500, 0, 180);
+		  printArray();
+		  pointArray[0] = mapAngle;
+		  servo1.writeMicroseconds(angle);
+		  delay(horizonDelay);
+		}
+		MoveUp();
+		// And back from 2500 to 0 degrees:
+		for (angle = angleTo; angle >= angleFrom; angle -= 7)
+		{
+		  pointArray[2] = SmoothLidarReading();
+		  mapAngle = angle;
+		  mapAngle = map(mapAngle, 500, 2500, 0, 180);
+		  printArray();
+		  pointArray[0] = mapAngle;
+		  servo1.writeMicroseconds(angle);
+		  delay(horizonDelay);
+		}
+		MoveUp();
+    }
+}
+
+void MoveUp() {
+  if (verAngle >= verAngleStop) {
+    verAngle = verAngle - 7;
+    servo2.writeMicroseconds(verAngle);
+    mapVerAngle = verAngle;
+    mapVerAngle = map(mapVerAngle, 500, 2500, 180, -90);
+    pointArray[1] = mapVerAngle;
+    delay(20);
+  }
 }
 
 int LidarReading()
 {
 	dist = tfmini.getDistance();
+	delay(10)
 	return dist;
 }
 
@@ -42,4 +112,14 @@ float SmoothLidarReading()
 	}
   averageDist = averageDist / SAMPLES;
   return averageDist;
+}
+
+void printArray() {
+  for (int i = 0; i < 3; i++) {
+    Serial.print(pointArray[i]);
+    if (i != 2) {
+      Serial.print(" ");
+    }
+  }
+  Serial.println();
 }
