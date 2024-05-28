1f0669ba20cef3e689d411b51b1df7560a632305
manojkhatri63 <168324053+manojkhatri63@users.noreply.github.com>
2024-05-28 19:50:33 +0530

    control.ino

--- ArduinoCode/control.ino
+++ ArduinoCode/control.ino
@@ -1,151 +1,111 @@
-#include<Servo.h>
-#include<SoftwareSerial.h>
-
-
-#define LIDAR_MIN_DIST 20
-#define LIDAR_MAX_DIST 800 // in cm
+#include <PWMServo.h>
+#include <SoftwareSerial.h>
+#define LIDAR_MIN_DIST 5
+#define LIDAR_MAX_DIST 900 // in cm
 #define SAMPLES 5
 
-SoftwareSerial Serial1(2,3); //define software serial port name as Serial1 and define pin2 as RX and pin3 as TX
-int dist; //actual distance measurements of LiDAR
-int strength; //signal strength of LiDAR
-float temprature;
-int check;  //save check value
+SoftwareSerial Serial1(2, 3);
+PWMServo hServo;  // Create servo object for horizontal servo
+PWMServo vServo;    // Create servo object for vertical servo
+
+const int HEADER = 0x59;
+int dist;
 int i;
-int uart[9];  //save data measured by LiDAR
-const int HEADER=0x59;  //frame header of data package
-
-
-Servo servoAlpha; //Vertial servo
-Servo servoTheta; //Horizontal servo
-
-int i;
-int pointArray[3]; //Holds the 0. hAngle 1. vAngle 2. distance
-
-int angle; 
-int verAngle; 
-int verAngleStop;
-int angleFrom; 
-int angleTo;
+int check;
+int pointArray[2];//r, theta, phi
+int uart[9];
 int mapAngle;
+int mapVerAngle;
+int hAngle;   // Variable to store horizontal servo position
+int vAngle = 180;     // Variable to store vertical servo position
+//int horizonDelay = 50;
 
-
-void setup()
-{
-	Serial.begin(9600); //set bit rate of serial port connecting Arduino with computer
-  	Serial1.begin(115200);  //set bit rate of serial port connecting LiDAR with Arduino
-	servoAlpha.attach(9); // attaches the vertical servo on pin 9 to the servo object
-	servoTheta.attach(10); // attaches the horizontal servo on pin 10 to the servo object
-	
-	//Horizontal Servo motion on startup
-	servoTheta.writeMicroseconds(2000);//left
-	delay(950);
-	servoTheta.writeMicroseconds(1000);//right
-	delay(950);
-	servoTheta.writeMicroseconds(1500);//middle
-	delay(950);
-	//Vertical servo motion on startup
-	servoAlpha.writeMicroseconds(1500);
-	delay(950);
-	servoAlpha.writeMicroseconds(1700);
-	delay(950);
-	servoAlpha.writeMicroseconds(1000);
-	delay(950);
-	
-	
+void setup() {
+  Serial1.begin(115200);
+  Serial.begin(115200);
+  hServo.attach(10);  // Attach horizontal servo to pin 9
+  vServo.attach(9);    // Attach vertical servo to pin 10
+  vServo.write(180);
+  hServo.write(vAngle);
 }
 
-void loop()
-{
-	
-	if (verAngle >= verAngleStop) 
-	{
-		for (angle = angleFrom; angle <= angleTo; angle += 7)
-		{
-		  pointArray[2] = SmoothLidarReading();
-		  mapAngle = angle;
-		  mapAngle = map(mapAngle, 500, 2500, 0, 180);
-		  printArray();
-		  pointArray[0] = mapAngle;
-		  servo1.writeMicroseconds(angle);
-		  delay(horizonDelay);
-		}
-		MoveUp();
-		// And back from 2500 to 0 degrees:
-		for (angle = angleTo; angle >= angleFrom; angle -= 7)
-		{
-		  pointArray[2] = SmoothLidarReading();
-		  mapAngle = angle;
-		  mapAngle = map(mapAngle, 500, 2500, 0, 180);
-		  printArray();
-		  pointArray[0] = mapAngle;
-		  servo1.writeMicroseconds(angle);
-		  delay(horizonDelay);
-		}
-		MoveUp();
+void loop() {
+  
+  if (vAngle <= 180){
+    for(hAngle = 0; hAngle <= 180; hAngle++)
+    {
+      pointArray[2] = SmoothLidarReading();
+      pointArray[0] = hAngle;
+      printArray();
+      hServo.write(hAngle);
+      delay(200);
+      
     }
+    moveUp();
+    for(hAngle = 180; hAngle >= 0; hAngle-=1)
+    {
+
+      pointArray[2] = SmoothLidarReading();
+      pointArray[0] = hAngle;
+      printArray();
+      hServo.write(hAngle);
+      delay(200);
+      
+    }
+    moveUp();
+  }
+
 }
 
-void MoveUp() 
-{
-  if (verAngle >= verAngleStop) 
-  {
-    verAngle = verAngle - 7;
-    servo2.writeMicroseconds(verAngle);
-    mapVerAngle = verAngle;
-    mapVerAngle = map(mapVerAngle, 500, 2500, 180, -90);
-    pointArray[1] = mapVerAngle;
-    delay(20);
-  }
+void moveUp() {
+    if(vAngle <= 180 ){
+      vAngle -= 5;
+      mapVerAngle = map(vAngle, 180, 90, 0, 90);
+      vServo.write(vAngle);
+      pointArray[1] = mapVerAngle;
+      delay(200);
+    }
 }
 
 int LidarReading()
 {
-	if (Serial1.available())
-	{  //check if serial port has data input
-    	if(Serial1.read() == HEADER) 
-	{  //assess data package frame header 0x59
-      		uart[0]=HEADER;
-      	if (Serial1.read() == HEADER) 
-      	{ //assess data package frame header 0x59
+  if (Serial1.available()) {  //check if serial port has data input
+    if(Serial1.read() == HEADER) {  //assess data package frame header 0x59
+      uart[0]=HEADER;
+      if (Serial1.read() == HEADER) { //assess data package frame header 0x59
         uart[1] = HEADER;
         for (i = 2; i < 9; i++) { //save data in array
           uart[i] = Serial1.read();
         }
         check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-        if (uart[8] == (check & 0xff))
-	{ //verify the received data as per protocol
+        if (uart[8] == (check & 0xff)){ //verify the received data as per protocol
           dist = uart[2] + uart[3] * 256;     //calculate distance value
-         return dist;
+          return dist;
         }
       }
     }
   }
+  delay(10);
+  return -1;
 }
 
-float SmoothLidarReading()
-{
-	float averageDist = 0;
-	for (int i = 0; i < SAMPLES; i = i + 1) 
-	{
-		int x = float(LidarReading());
-		while (x < LIDAR_MIN_DIST || x > LIDAR_MAX_DIST) 
-		{
-		  x = float(LidarReading());
-		}
-		averageDist = averageDist + x;
-	}
+float SmoothLidarReading() {
+  float averageDist = 0;
+  for (int i = 0; i < SAMPLES; i = i + 1) {
+    int x = float(LidarReading());
+    while (x < LIDAR_MIN_DIST || x > LIDAR_MAX_DIST) {
+      x = float(LidarReading());
+    }
+    averageDist = averageDist + x;
+  }
   averageDist = averageDist / SAMPLES;
   return averageDist;
 }
 
-void printArray() 
-{
-  for (int i = 0; i < 3; i++)
-  {
+void printArray() {
+  for (int i = 0; i < 3; i++) {
     Serial.print(pointArray[i]);
-    if (i != 2) 
-    {
+    if (i != 3) {
       Serial.print(" ");
     }
   }
